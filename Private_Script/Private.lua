a63ccb4c51826de8d19d8c1ac51441c172f56095
New Hacker Boy <61928133+NewHackerBoy@users.noreply.github.com>
2020-03-08 15:07:40 +0530

    Create Private.lua

---
+++ Private_Script/Private.lua
@@ -0,0 +1,216 @@
+
+gg.alert("ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ sᴡᴀʀɢ ᴏғғɪᴄɪᴀʟ ᴘᴀɪᴅ sᴄʀɪᴘᴛ")
+gg.setVisible(false)
+function HOME()
+  MN = gg.choice({
+	"🔵 ʟᴏʙʙʏ ᴏɴᴄᴇ",
+    "🔴 ɢᴀᴍᴇ  ᴏɴᴄᴇ",
+    "🔚 ___ ᴇxɪᴛ ___"
+    
+    }, nil,"sᴡᴀʀɢ ᴏғғɪᴄɪᴀʟ ᴘᴀɪᴅ sᴄʀɪᴘᴛ")
+    if MN == nil then
+  else
+    if MN == 1 then
+      LBY()
+    end
+    if MN == 2 then
+      GME()
+    end
+    if MN == 3 then
+      CLOSE()
+    end
+    end
+  PUBGMH = -1
+end
+    
+ function LBY()
+    LBYHCK = gg.multiChoice({
+    "🕴 ᴀɪᴍ ʙᴏᴛ",
+    "🕵 ᴀɪᴍ ʟᴏᴄᴋ",
+    "💯 ɴᴏ ʀᴇᴄᴏɪʟ",
+    "🔙 __ʙᴀᴄᴋ_"
+    
+    }, nil,"sᴡᴀʀɢ ᴏғғɪᴄɪᴀʟ ᴘᴀɪᴅ sᴄʀɪᴘᴛ")
+    if LBYHCK == nil then
+  else
+  
+    if LBYHCK[1] == true then
+      AMBT()
+    end
+    if LBYHCK[2] == true then
+      AMLK()
+    end
+    if LBYHCK[3] == true then
+      NRCL()
+    end
+    if LBYHCK[4] == true then
+      HOME()
+    end
+  end
+  PUBGMH = -1
+end
+
+function AMBT()
+  gg.clearResults()
+  gg.setRanges(gg.REGION_C_DATA)
+  gg.setRanges(gg.REGION_C_DATA)
+  gg.searchNumber("360;0.0001;1478828288", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("0.0001", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(100)
+  gg.editAll("9999", gg.TYPE_FLOAT)
+  gg.toast("🕴️ᴀɪᴍ ʙᴏᴛ ᴀᴄᴛɪᴠᴀᴛᴇᴅ🕴️")
+end
+
+function AMLK()
+  gg.clearResults()
+  gg.setRanges(gg.REGION_C_DATA)
+  gg.setRanges(gg.REGION_C_DATA)
+  gg.searchNumber("360;0.0001;1478828288", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("0.0001", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(100)
+  gg.editAll("999999999", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.toast("🕵 ᴀɪᴍ ʟᴏᴄᴋ ᴀᴄᴛɪᴠᴀᴛᴇᴅ🕵")
+end
+
+function NRCL()
+  gg.clearResults()
+  gg.setRanges(8)
+  gg.searchNumber("-2.786982e28;-3.7444097e28;-1.1368837e-13::", 16, false, 536870912, 0, -1)
+  gg.searchNumber("-3.7444097e28", 16, false, 536870912, 0, -1)
+  gg.getResults(1)
+  gg.editAll("0", 16)
+  gg.clearResults()
+  gg.setRanges(8)
+  gg.searchNumber("-2.8111605e28;-3.7444097e28;-1.1144502e28;128.0::", 16, false, 536870912, 0, -1)
+  gg.searchNumber("-1.1144502e28", 16, false, 536870912, 0, -1)
+  gg.getResults(1)
+  gg.editAll("0", 16)
+  gg.clearResults()
+  gg.setRanges(8)
+  gg.searchNumber("-6.1549454e27;1.8638966e-20;-1.1144502e28;0::", 16, false, 536870912, 0, -1)
+  gg.searchNumber("-1.1144502e28", 16, false, 536870912, 0, -1)
+  gg.getResults(1)
+  gg.editAll("0", 16)
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("1,348,759,109;1953067887;1,634,692,166;1,920,287,604::28", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1634692166", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(10)
+  gg.editAll("1634692266", gg.TYPE_DWORD)
+  gg.toast("💯 ɴᴏ ʀᴇᴄᴏɪʟ ᴀᴄᴛɪᴠᴀᴛᴇᴅ 💯")
+end
+
+
+function GME()
+GMEHCK = gg.multiChoice({
+	"♨️ ᴘᴀʀᴀsᴜᴛ ᴠɪᴘ",	
+    "🗼 ᴀɴᴛᴇɴᴀ ᴘʀᴏ",
+    "🌿 ᴄᴏʟᴏʀ ʙᴇsᴛ",
+    "🕵️ ʜᴇᴀᴅ sʜᴏᴛ",
+    "💯 ᴘʀᴏ ʙᴜʟʟᴇᴛ",
+    "🔙 ___ʙᴀᴄᴋ___"
+    
+    }, nil,"sᴡᴀʀɢ ᴏғғɪᴄɪᴀʟ ᴘᴀɪᴅ sᴄʀɪᴘᴛ")
+    if GMEHCK == nil then
+  else
+  if GMEHCK[1] == true then
+      FSTPRST()
+    end
+    if GMEHCK[2] == true then
+      ANTN()
+    end
+    if GMEHCK[3] == true then
+      MCLR()
+    end
+    if GMEHCK[4] == true then
+      HDST()
+    end
+    if GMEHCK[5] == true then
+      PRBLT()
+    end
+    if GMEHCK[6] == true then
+      HOME()
+    end
+  end
+  PUBGMH = -1
+end
+
+function FSTPRST()
+   gg.clearResults()
+   gg.setRanges(gg.REGION_ANONYMOUS)
+   gg.searchNumber('0.75;150;1', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+   gg.searchNumber('1', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+   gg.getResults(100)
+   gg.editAll('30', gg.TYPE_FLOAT)
+   gg.clearResults()
+   gg.setRanges(gg.REGION_ANONYMOUS)
+   gg.searchNumber('0.75;150;30', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+   gg.searchNumber('0.75', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+   gg.getResults(100)
+   gg.editAll('0', gg.TYPE_FLOAT)
+   gg.clearResults()
+   gg.toast('♨️ ғᴀsᴛ ᴘᴀʀᴀsᴜᴛᴇ ᴀᴄᴛɪᴠᴀᴛᴇᴅ ♨️')
+end
+
+function ANTN()
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("88.50576019287F;87.27782440186F;-100.91194152832F;1F::13", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("88.50576019287F;87.27782440186F;1F", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(6)
+  gg.editAll("1.96875;1.96875;999;1.96875;1.96875;999", gg.TYPE_FLOAT)--50000
+  gg.clearResults()
+  gg.toast("🗼 ᴀɴᴛᴇɴᴀ ᴘʀᴏ ᴀᴄᴛɪᴠᴀᴛᴇᴅ🗼")
+end
+
+function MCLR()
+  gg.clearResults()
+  gg.setRanges(32)
+  gg.searchNumber("573.70306396484;0.05499718338;1::50", 16, false, 536870912, 0, -1)
+  gg.searchNumber("1", 16, false, 536870912, 0, -1)
+  gg.getResults(1)
+  gg.editAll("999", 16)
+  gg.clearResults()
+  gg.toast('🌿 ᴠɪᴘ ᴄᴏʟᴏʀ ᴀᴄᴛɪᴠᴀᴛᴇᴅ 🌿')
+end
+
+function HDST()
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("9.20161819458;23;25;30.5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("25;30.5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(10)
+  gg.editAll("300", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.toast("🕵️ ᴀᴜᴛᴏ ʜᴇᴀᴅsʜᴏᴛ ᴀᴄᴛɪᴠᴀᴛᴇᴅ 🕵️")
+end
+
+function PRBLT()
+    gg.clearResults()
+gg.searchNumber("1,048,130,372D;16;12.66705417633;1,119,194,409D;1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("16", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(3)
+gg.editAll("121", gg.TYPE_FLOAT)
+gg.clearResults()
+  gg.toast("💯 ᴘʀᴏ ʙᴜʟʟᴇᴛ ᴀᴄᴛɪᴠᴀᴛᴇᴅ 💯")
+end
+
+function CLOSE()
+    print('☆【sᴡᴀʀɢ_ᴏғғɪᴄɪᴀʟ】☆')
+    gg.setVisible(true)
+    os.exit()
+  end
+  while true do
+    if gg.isVisible(true) then
+      PUBGMH = 1
+      gg.setVisible(false)
+    end
+    gg.clearResults()
+    if PUBGMH == 1 then
+      HOME()
+    end
+  end
+
